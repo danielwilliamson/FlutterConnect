2bfff31039724cb7d1eae7c015abd0ea5335a19a
Eddie Foy <edwardfoyiii@gmail.com>
2019-06-01 16:38:11 +1000

    Changes to ProfileList and adding UserSearch to Main

--- flutter_connect/lib/main.dart
+++ flutter_connect/lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_connect/profile_list.dart';
+
+import 'model/user.dart';
 
 void main() => runApp(MyApp());
 
@@ -6,6 +10,8 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+//    getFirestoreInfo();
+
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
@@ -23,6 +29,20 @@ class MyApp extends StatelessWidget {
       home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
+}
+
+Future getFirestoreInfo() async {
+  Query query = Firestore.instance.collection("users");
+
+  QuerySnapshot querySnapshot = await query.getDocuments();
+  print("query gotten  ${querySnapshot.documents.length}");
+
+  querySnapshot.documents.forEach((doc) {
+    print("Doc");
+    Map<String, dynamic> data = doc.data;
+    print("name = ${data["name"]}");
+  });
+  print("end");
 }
 
 class MyHomePage extends StatefulWidget {
@@ -45,6 +65,14 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  List<User> users;
+
+  @override
+  void initState() {
+    users = List<User>();
+    // TODO: implement initState
+    super.initState();
+  }
 
   void _incrementCounter() {
     setState(() {
@@ -59,53 +87,124 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          title: Text(widget.title),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () => showSearch(
+                    context: context,
+                    delegate: UserSearch(
+                      users: users,
+//                      onUserSelected:,
+                    ),
+                  ),
+            ),
+          ]),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
+        child: StreamBuilder<QuerySnapshot>(
+            stream: Firestore.instance.collection('users').snapshots(),
+            builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+              if (snapshot.hasError)
+                return new Text('Error: ${snapshot.error}');
+              switch (snapshot.connectionState) {
+                case ConnectionState.waiting:
+                  return new Text('Loading...');
+                default:
+                  if(snapshot.hasData) {
+                    List<User> newListOfUsers = List<User>();
+                    List<DocumentSnapshot> userSnapshots = snapshot.data
+                        .documents;
+                    userSnapshots.forEach((DocumentSnapshot documentSnapshot) {
+
+                      User user = User.fromJson(documentSnapshot.data);
+                      if(user != null) {
+                        newListOfUsers.add(user);
+                      }
+                    });
+                    users = newListOfUsers;
+                    return ProfileList(userSnapshots: userSnapshots);
+                  } else {
+                    return Container();
+                  }
+              }
+            }),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ), //
+      // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class UserSearch extends SearchDelegate<User> {
+  final List<User> users;
+//  final ValueSetter onUserSelected;
+
+  UserSearch({
+    this.users,
+//    this.onUserSelected,
+  });
+
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    return [IconButton(icon: Icon(Icons.clear), onPressed: () => query = "")];
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    return IconButton(
+        icon: Icon(Icons.arrow_back), onPressed: () => close(context, null));
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    return Container();
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    if (users == null) {
+      return Center(child: Text("USERS = NULL!"));
+    } else if (users.isEmpty) {
+      return Center(child: Text("No USERS! ${users.length}"));
+    }
+    final results = query.isEmpty
+        ? users
+        : users.where(
+            (user) => user.name.toLowerCase().contains(query.toLowerCase()));
+    return Padding(
+      padding: const EdgeInsets.all(32.0),
+      child: ListView(
+        shrinkWrap: true,
+        children: results
+            .map<Widget>(
+              (user) => Card(
+                    child: ListTile(
+                      contentPadding: EdgeInsets.fromLTRB(32.0, 8.0, 8.0, 8.0),
+                      title: Text(
+                        user.name,
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 18),
+                      ),
+                      subtitle: Text("${user.email}"),
+                      leading: Icon(Icons.person),
+                      onTap: () {
+//                        onUserSelected(null);
+                        close(context, null);
+                      },
+                    ),
+                  ),
+            )
+            .toList(),
+      ),
     );
   }
 }
